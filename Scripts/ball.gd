31ae63b270ce8b5a50e3a1b869cc6b7782a4a648
Matthew Pike (s3865305) <80456170+s3865305@users.noreply.github.com>
2024-03-17 17:11:27 +1100

    Finished basic full implementation

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -3,15 +3,12 @@ extends RigidBody2D
 @export var speed = 500
 
 var dir: Vector2
+var spawn_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var velocity = Vector2.ZERO
-	var random_velocity = [-1, 1]
-	velocity.x = random_velocity.pick_random()
-	velocity.y = random_velocity.pick_random()
-	
-	dir = velocity
+	spawn_position = position
+	generate_new_ball()
 
 func _physics_process(delta):
 	var collision = move_and_collide(dir * delta * speed)
@@ -19,3 +16,12 @@ func _physics_process(delta):
 	if collision:
 		collider = collision.get_collider()
 		dir = dir.bounce(collision.get_normal())
+	
+func generate_new_ball():
+	position = spawn_position
+	var velocity = Vector2.ZERO
+	var random_velocity = [-1, 1]
+	velocity.x = random_velocity.pick_random()
+	velocity.y = random_velocity.pick_random()
+	
+	dir = velocity
--- Scripts/pong.gd
+++ Scripts/pong.gd
@@ -11,3 +11,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_player_1_scored(body):
+	player1_score += 1
+	$HUD/Score1.text = str(player1_score)
+	
+	if player1_score == 11:
+		$HUD/WinnerMessage/Message.text = "Player 1 Wins!"
+		$HUD/WinnerMessage.visible = true
+	else:
+		$Ball.generate_new_ball()
+
+func _on_player_2_scored(body):
+	player2_score += 1
+	$HUD/Score2.text = str(player2_score)
+	
+	if player2_score == 11:
+		$HUD/WinnerMessage/Message.text = "Player 2 Wins!"
+		$HUD/WinnerMessage.visible = true
+	else:
+		$Ball.generate_new_ball()
